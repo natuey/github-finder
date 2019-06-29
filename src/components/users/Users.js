f45a98c76cfa2f39902c83d20e5b052a59ae99ce
Nathan Tu <nathantuey@gmail.com>
2019-06-29 23:36:30 +1000

    Refactor using Hook and Reducer and Context

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+    const githubContext = useContext(GithubContext)
+
+    const { loading, users } = githubContext;
 
     if (loading) {
         return <Spinner />;
@@ -26,4 +29,4 @@ const userStyle = {
 };
 
 
-export default Users
+export default Users;
